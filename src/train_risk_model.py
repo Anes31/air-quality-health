0242d1e2e9161ac74ffe46f545a9150911428c2e
Anes <anes.wahran@gmail.com>
2025-11-19 16:33:37 -0500

    clean train script

--- src/train_risk_model.py
+++ src/train_risk_model.py
@@ -1,6 +1,5 @@
 import os
 import pandas as pd
-from sklearn.model_selection import train_test_split
 from lightgbm import LGBMRegressor
 from sklearn.metrics import mean_squared_error
 import numpy as np
